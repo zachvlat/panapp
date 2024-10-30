0713d94ea0db8ec4c5579c1b9d5ef65bc8401a3a
zachvlat <zachariasvlatakis@gmail.com>
2024-10-30 16:00:19 +0200

    sadsa

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Sport24News from './Sport24News';
 import SdnaNews from './SdnaNews'
+import GazzettaNews from './GazzettaNews'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Sport24News></Sport24News>
       <SdnaNews></SdnaNews>
+      <GazzettaNews></GazzettaNews>
     </View>
   );
 }
