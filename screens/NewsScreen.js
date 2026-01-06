288176979cd9d00618848911cdd87aec10566c40
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2026-01-06 22:58:25 +0200

    Update RSS feed URL for Panathinaikos

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -4,7 +4,7 @@ import { Text, useTheme, FAB } from 'react-native-paper';
 import NewsComponent from '../components/NewsComponent';
 
 const rssFeeds = [
-  'https://www.gazzetta.gr/teams/panathinaikos/rss',
+  'https://corsproxy.io/?https://www.gazzetta.gr/teams/panathinaikos/rss',
   'https://corsproxy.io/?https://www.inpao.gr/feed/',
   'https://corsproxy.io/?https://www.sdna.gr/latest.xml',
   'https://corsproxy.io/?https://leoforos1908.gr/?feed=rss2',
